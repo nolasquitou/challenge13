c8333cf57bf6f323ca69faf8a7e4a0c1a2120079
Nolasquito <nolascopalavecino.com>
2023-04-02 01:05:41 -0300

    Se guardo el proyecto en esta rama

---
+++ app.js
@@ -0,0 +1,36 @@
+// Ejercicio 1
+
+let saludo = "Hola ";
+let saludo2 = "como estas?";
+document.write (saludo + saludo2);
+
+// Ejercicio 2 
+
+// variables
+
+let numero1 = 10;
+let numero2 = 5;
+let numero3 = 20;
+
+// suma, resta, division, multiplicacion
+
+console.log (suma = (numero1 + numero2));
+console.log (resta = (numero1 - numero2));
+console.log (division = (numero1 / numero2));
+console.log (producto = (numero1 * numero2));
+
+// par o inpar
+
+console.log (` ${(numero1 %2) == 0 ? "Es par" : "Es inpar"}`)
+console.log (` ${(numero2 %2) == 0 ? "Es par" : "Es inpar"}`)
+
+// Ejercicio 3 
+
+console.log(suma + numero3);
+console.log(producto * numero3);
+console.log(resta - numero3);
+
+
+
+
+
